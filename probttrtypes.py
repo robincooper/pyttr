9fe525dc4c89ea25ed44221a2926d446ea7ece5d
Robin Cooper <robinhcooper@gmail.com>
2021-01-28 17:32:42 +0100

    PConstraint

--- probttrtypes.py
+++ probttrtypes.py
@@ -1,5 +1,37 @@
 import ttrtypes
 
+class PConstraint:
+    def __init__(self,n,m=None):
+        self.min = float(n)
+        if m is None:
+            self.max = self.min
+        else:
+            self.max = m
+    def show(self):
+        if self.max == self.min:
+            return str(self.min)
+        elif self.min == 0 and self.max == 1:
+            return 'no constraint'
+        elif self.max == 1:
+            return '>='+str(self.min)
+        elif self.min == 0:
+            return '<='+str(self.max)
+        else:
+            return 'p such that '+str(self.min)+'<=p<='+str(self.max)
+    def to_latex(self,vars=[]):
+        if self.max == self.min:
+            return str(self.min)
+        elif self.min == 0 and self.max == 1:
+            return '\mathrm{no\ constraint}'
+        elif self.max == 1:
+            return '\geq'+str(self.min)
+        elif self.min == 0:
+            return '\leq'+str(self.max)
+        else:
+            return 'p \mathrm{\ such\ that\ }'+str(self.min)+'\leq p\leq'+str(self.max)
+    def match(self,p):
+        return p.min<=self.min and self.max<=p.max
+
 class Type(ttrtypes.Type):
     def __init__(self,name='',cs={}):
         super().__init__(name,cs)
@@ -9,14 +41,15 @@ class Type(ttrtypes.Type):
             return True
         elif a in self.witness_cache[0] and isinstance(a,str):
             return True
-        elif next((c for c in self.witness_conditions if c(a) is p),False):
+        elif next((c for c in self.witness_conditions if p.match(c(a))),False):
             return True
         else:
             return False
-    def judge(self, a, p):
+    def judge(self, a, n, max=None):
+        p = PConstraint(n,max)
         if a in self.witness_cache[0]:
-            self.witness_cache[1][self.witness_cache[0].index(a)] = p
-            return p
+           self.witness_cache[1][self.witness_cache[0].index(a)] = p
+           return p
         elif isinstance(a,str):
             self.witness_cache[0].append(a)
             self.witness_cache[1].append(p)
@@ -27,5 +60,4 @@ class Type(ttrtypes.Type):
             return p
         else: return False
         
-
 

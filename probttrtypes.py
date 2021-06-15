b3bc96ddd5522c716dcc06dc8676227a187c7a52
Robin Cooper <robinhcooper@gmail.com>
2021-06-15 13:54:49 +0200

    beginning probttrbayes

--- probttrtypes.py
+++ probttrtypes.py
@@ -706,6 +706,34 @@ class NegType(TypeClass):
         return PNeg(self.comps.base_type.query_nonspec(c,oracle))
     def query_doublecond(self,c:'python list of types',oracle=None):
         return PNeg(self.comps.base_type.query_doublecond(c,oracle))
+
+class VarType(TypeClass):
+    def __init__(self,Ts:'list of types'):
+        self.comps = Rec({'value_types':Ts})
+    def learn_witness_condition(self,c):
+        logtype(self,c)
+    def learn_witness_type(self,c):
+        logtype_t(self,c)
+    def judge(self,a,n=1,max=None):
+        if ttracing('judge'):
+            print(show(self)+' is a variable type and cannot be judged')
+    def judge_nonspec(self,n=1,max=None):
+        if ttracing('judge_nonspec'):
+            print(show(self)+' is a variable type and cannot be judged')
+    def query(self,a,c=[],oracle=None):
+        return PSum([self.query_v(a,T,c,oracle) for T in self.comps.value_types])
+    def query_v(self,a,T,c=[],oracle=None):
+        if T in self.comps.value_types:
+            denominator = PSum([A.query(a,c,oracle) for A in self.comps.value_types])
+            return PDiv(T.query(a,c,oracle),denominator)
+        else:
+           supertypes = map(lambda T1 : T.subtype_of(T1), self.comps.value_types) 
+           if any(supertypes)  and any(supertypes):
+               return PConstraint(0)
+           else:
+               return PConstraint(0,1)
+        
+        
 #--------------------
 # Probability classes
 #--------------------
@@ -778,8 +806,14 @@ def PExtreme(plist):
     if plist:
         return PConstraint(min(map(lambda p: p.min, plist)),
                            max(map(lambda p: p.max, plist)))
+def PSum(plist):
+    if plist:
+        return PConstraint(sum(map(lambda p: p.min, plist)),
+                          sum(map(lambda p: p.max, plist)))
 def PNeg(p):
     return PConstraint(1-p.max,1-p.min)
+def PRound(p,n=10):
+    return PConstraint(round(p.min,n),round(p.max,n))
 def PTimes(p1,p2):
     return PConstraint(p1.min*p2.min,p1.max*p2.max)
 def PDiv(p1,p2):
